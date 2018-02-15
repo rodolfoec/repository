9441f877c858408fd14ac7b2f5c7181f659a5709
Rodolfo Estrada <rodolfoestrada@la-MacBook-Pro-de-Rodolfo.local>
2018-02-15 15:50:56 -0600

    New

--- JS/pagina.js
+++ JS/pagina.js
@@ -1,9 +1,8 @@
-function	fResponsive(){
-	var x =document.getElementById("navBar");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	}
-	else{
-		x.className = "topnav";
-	}
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
